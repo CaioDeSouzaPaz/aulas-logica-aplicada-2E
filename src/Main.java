bf3fd0b8a66068e054c3400d6b26ed1e918b3f30
rm86706 <caiotst98@gmail.com>
2023-05-18 12:26:05 -0300

    aula de introdução 002
    aprendi tudo que me foi proposto

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,14 @@ public class Main {
     public static void main(String[] args) {
         //aula introdutória de Orientação á Objetos
         Pessoa adao = new Pessoa();
+        //Declaração de objeto
+        Pessoa qualquer;
+        //instanciação de objeto
+        qualquer = new Pessoa();
+        //Definiçao do formato pessoa qualquer
+        qualquer.nome = "rita";
+        qualquer.sobrenome = "lee";
+        qualquer.falar();
+
     }
 }
--- src/Pessoa.java
+++ src/Pessoa.java
@@ -2,5 +2,8 @@ public class Pessoa {
     //atributos da pessoa
     public String nome;
     public String sobrenome;
-
+    //Método da pessoa
+    public void falar (){
+        System.out.println("falei");
+    }
 }
